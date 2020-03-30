712ec79afaaa7dd3c6d7aa12e5eb15396fe71adb
katerina0510 <ketrin_tim@bk.ru>
2020-03-30 17:37:10 +0300

    добавлены файлы для решения задачи 3.6 шаг 9

--- test_items.py
+++ test_items.py
@@ -2,7 +2,7 @@ import time
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
-def test_item(browser):
+def test_find_a_button(browser):
 	browser.get(link)
 	button_count = len(browser.find_elements_by_css_selector("#add_to_basket_form > button"))
 	time.sleep(5)
